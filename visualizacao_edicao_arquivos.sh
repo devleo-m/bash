d75e60aeca092d075885618f00948e657ac95d88
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-06-20 01:23:32 -0300

    Create visualizacao_edicao_arquivos.sh

---
+++ visualizacao_edicao_arquivos.sh
@@ -0,0 +1,11 @@
+# cat: Exibir o conteúdo de arquivos.
+cat nome_arquivo
+
+# more e less: Visualizar arquivos longos.
+more nome_arquivo
+less nome_arquivo
+
+# nano: Editor de texto básico.
+nano nome_arquivo
+
+

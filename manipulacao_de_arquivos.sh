991f24a1590d706bac4d7d22b8e93a15f5afbf40
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-06-20 01:23:40 -0300

    Create manipulacao_de_arquivos.sh

---
+++ manipulacao_de_arquivos.sh
@@ -0,0 +1,23 @@
+# mkdir: Criar diretórios.
+mkdir nome_diretorio
+
+# rmdir: Remover diretórios vazios.
+rmdir nome_diretorio
+
+# touch: Criar arquivos vazios.
+touch nome_arquivo
+
+# cp: Copiar arquivos ou diretórios.
+cp arquivo_origem arquivo_destino
+cp -r diretorio_origem diretorio_destino
+
+# mv: Mover ou renomear arquivos ou diretórios.
+mv arquivo_origem arquivo_destino
+mv nome_antigo novo_nome
+
+# rm: Remover arquivos ou diretórios.
+rm nome_arquivo
+rm -r nome_diretorio
+
+
+
